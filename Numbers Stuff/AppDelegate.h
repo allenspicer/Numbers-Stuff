efb14f84434858edfaf83b88b4f83b47bab5c65c
Allen Spicer <allenspicer@gmail.com>
2016-05-11 09:11:24 -0400

    Initial Commit

---
+++ Numbers Stuff/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Numbers Stuff
+//
+//  Created by Allen Spicer on 5/11/16.
+//  Copyright © 2016 Allen Spicer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Numbers Stuff/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Numbers Stuff
+//
+//  Created by Allen Spicer on 5/11/16.
+//  Copyright © 2016 Allen Spicer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
